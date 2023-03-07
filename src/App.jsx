275701e8e4884c4d9e2492d9798a898743b7e100
RomiMolina <roomi.molina@gmail.com>
2023-03-07 15:18:35 -0300

    feat: componente con mi nombre

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import { Romina } from './components/Romina'
 
 function App() {
 
   return (
     <div className="App">
      <h1>Hello word</h1>
+     <Romina/>
     </div>
   )
 }
