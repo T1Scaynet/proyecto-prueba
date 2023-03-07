188f37102f44446f0beb7949694ba9f54c6ab464
Ramiro Aduviri <Kevin_95_ah@hotmail.com.ar>
2023-03-07 15:11:03 -0300

    feat: component Rama created

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import { Rama } from './components/Rama'
 
 function App() {
 
   return (
     <div className="App">
      <h1>Hello word</h1>
+     <Rama />
     </div>
   )
 }
---
+++ src/components/Rama.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Rama = () => {
+  return (
+    <div>Rama</div>
+  )
+}
