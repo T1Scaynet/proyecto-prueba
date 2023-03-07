dcab142a89ee106f52060e3e6fcdadb781d00e21
joacorojasmoschini <joaco.vla@gmail.com>
2023-03-07 15:37:38 -0300

    feat:cree mi componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Joaco from './components/joaco'
 
 function App() {
 
   return (
     <div className="App">
      <h1>Hello word</h1>
+     <Joaco/>
     </div>
   )
 }
