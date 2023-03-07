b0fa20dff0589e9ee381907480306d986ddd8b13
rojassrodrigo <rojasrodrigo@live.com>
2023-03-07 15:26:13 -0300

    feat: cree mi componente con mi nombre

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Rodrigo from './components/Rodrigo'
 
 function App() {
 
   return (
     <div className="App">
      <h1>Hello word</h1>
+     <Rodrigo/>
     </div>
   )
 }
