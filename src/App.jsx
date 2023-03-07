8fc5c34108bb6fc9aac0ecbdc5daf6766a9589a8
Gustavo Chura <gustavochura94@gmail.com>
2023-03-07 20:35:47 -0300

    Fix: Components order

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
 import './App.css'
-import Joaco from './components/joaco'
+import Joaco from './components/Joaco'
 import Rodrigo from './components/Rodrigo'
 import { Romina } from './components/Romina'
 import { Rama } from './components/Rama'
 import { Selene } from './components/Selene'
 
-
 function App() {
-
   return (
     <div className="App">
-     <h1>Hello word</h1>
+     <h1>Hello Team</h1>
      <Joaco/>
      <Rodrigo/>
      <Romina/>
