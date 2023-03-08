d239927332de4062cd6bbd633faf0fb5a7819a84
Gustavo Chura <gustavochura94@gmail.com>
2023-03-07 21:37:51 -0300

    feat: added new components

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,27 @@
-import './App.css'
-import Joaco from './components/Joaco'
-import Rodrigo from './components/Rodrigo'
-import { Romina } from './components/Romina'
-import { Rama } from './components/Rama'
-import { Selene } from './components/Selene'
+import "./App.css";
+import { Joaco } from "./components/Joaco";
+import { Rodrigo } from "./components/Rodrigo";
+import { Romina } from "./components/Romina";
+import { Rama } from "./components/Rama";
+import { Selene } from "./components/Selene";
+import { Tani } from "./components/Tani";
+import { Ivan } from "./components/Ivan";
+import { Gustavo } from "./components/Gustavo";
 
 function App() {
   return (
     <div className="App">
-     <h1>Hello Team</h1>
-     <Joaco/>
-     <Rodrigo/>
-     <Romina/>
-     <Rama />
-     <Selene/>
+      <h1>Hello Team</h1>
+      <Joaco />
+      <Rodrigo />
+      <Romina />
+      <Rama />
+      <Selene />
+      <Tani/>
+      <Ivan/>
+      <Gustavo/>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
